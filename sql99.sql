097810a4be24f9c621e2b3e5cf1541dcd6222e83
Jhon <34300982+JohnComeon@users.noreply.github.com>
2021-12-26 15:58:47 +0800

    Add files via upload

--- sql99.sql
+++ sql99.sql
@@ -34,6 +34,7 @@ on 连接条件；
     非等值
     自连接
 
+inner可以省略
 筛选条件放在where后面，连接条件放在on后面，提高了分离性，便于阅读。
 inner join连接和sql92语法中的等值连接效果是一样的，都是查询多表的交集。
 
@@ -62,7 +63,7 @@ INNER JOIN departments d
 ON l.`location_id`=d.`location_id`
 GROUP BY city
 HAVING 部门个数>3;
-
+   
 # 查询哪个部门的部门员工个数>3的部门名和员工个数，并按个数降序
 SELECT department_name, COUNT(*) 员工个数
 FROM departments d
@@ -84,10 +85,98 @@ ORDER BY department_name DESC;
 
 
 ################二、非等值连接###############
-#
+
+#查询员工的工资级别
+SELECT salary,grade_level
+FROM job_grades g
+INNER JOIN employees e
+ON e.`salary` BETWEEN g.`lowest_sal` AND g.`highest_sal`;
 
 
-#
+#查询工资级别>20的个数，并且按工资级别降序
+SELECT COUNT(*),grade_level
+FROM job_grades g
+INNER JOIN employees e
+ON e.`salary` BETWEEN g.`lowest_sal` AND g.`highest_sal`
+GROUP BY grade_level
+HAVING COUNT(*)>20
+ORDER BY grade_level DESC;
+
+################非等值连接###############
+
+
+##################自连接###################
+#查询员工的名字、上级的名字
+SELECT e.last_name, m.last_name
+FROM employees e
+INNER JOIN employees m 
+ON e.`manager_id`=m.`employee_id`;
+
+#查询姓名中包含字符k的员工的名字、上级的名字
+SELECT e.last_name, m.last_name
+FROM employees e
+INNER JOIN employees m 
+ON e.`manager_id`=m.`employee_id`
+WHERE e.`last_name` LIKE '%k%';
+
+##################自连接###################
+
+
+####################外连接##################
+#应用场景：用于查询一个表中有，另一个表没有的记录
+特点：
+1、外连接的查询结果为主表中的所有记录
+	若从表中有和它匹配的，则显示匹配的值，否则显示NULL
+	外连接查询结果 = 内连接结果+主表中有而从表中没有的记录
+2、左外连接：LEFT左边的是主表
+   右外连接：RIGHT右边的是主表
+3、左外和右外交换两个表的顺序，可以实现同样的效果
+
+# 全外连接 = 内连接的结果+表1中有但表2没有的+表2中有但表1没有的 （内连接+左外+右外）
+
+SELECT * FROM beauty;
+SELECT * FROM boy;
+
+#查询男朋友不在男生表中的女生名
+SELECT b.name, bo.*
+FROM beauty b
+LEFT OUTER JOIN boys bo
+ON b.`boyfriend_id`=bo.`id`
+WHERE bo.`id` IS NULL;
+
+# 查询哪个部门没有员工
+#左外
+SELECT d.*, e.employee_id
+FROM departments d
+LEFT OUTER JOIN employees e
+ON d.`department_id` = e.`department_id`
+WHERE e.`employee_id` IS NULL;
+
+
+####################外连接##################
+
+
+####################交叉连接##################
+SELECT b.*, bo.*
+FROM beauty b
+CROSS JOIN boys bo;
+
+####################交叉连接##################
+
+
+# 查询哪个城市没有部门
+SELECT city
+FROM departments d
+RIGHT OUTER JOIN locations l
+ON d.`location_id` = l.`location_id`
+WHERE d.`location_id` IS NULL;
+
+# 查询部门名为SAL或IT的员工信息
+SELECT e.*,d.`department_name`
+FROM departments d
+LEFT JOIN employees e
+ON d.`department_id`= e.`department_id`
+WHERE d.`department_name` IN ('SAL', 'IT')
 
 
 
